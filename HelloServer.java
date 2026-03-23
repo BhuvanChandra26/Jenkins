ac7224b51c89f670b5d9856ac17a97f8d1cd775d
BhuvanChandra26 <bhuvanchandra2611@gmail.com>
2026-03-23 10:06:44 +0530

    okay

--- HelloServer.java
+++ HelloServer.java
@@ -8,18 +8,17 @@ public class HelloServer {
     public static void main(String[] args) throws IOException {
         int port = 8081; // change if needed
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-        server.createContext("/", HelloServer::handle);
+        // Use a lambda instead of method reference to avoid any parser confusion
+        server.createContext("/", (HttpExchange exchange) -> {
+            String response = "Hello World";
+            exchange.getResponseHeaders().add("Content-Type", "text/plain; charset=utf-8");
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        });
         server.setExecutor(null);
         System.out.println("Server running on http://0.0.0.0:" + port + "/");
         server.start();
-    }
-
-    private static void handle(HttpExchange exchange) throws IOException {
-        String response = "Hello World";
-        exchange.getResponseHeaders().add("Content-Type", "text/plain; charset=utf-8");
-        exchange.sendResponseHeaders(200, response.getBytes().length);
-        try (OutputStream os = exchange.getResponseBody()) {
-            os.write(response.getBytes());
-        }
     }
 }
